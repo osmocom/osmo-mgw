9a3543a8d4508d74119145b90b81aa40a824e1fa
Philipp Maier <pmaier@sysmocom.de>
2017-11-14 15:23:40 +0100

    cosmetic: use correct VTY port number constant
    osmo-mgw currently uses VTY port number constant of osmo-bsc_mgwp,
    however, libosmore now offers a constant specifically for osmo_mgw,
    which has the same value, but is intended to be used by osmo-mgw
    
    use the new port number constant for osmo-mgw
    
    Closes: OS#2628
    Change-Id: I63c3b300cc9287d1755a3f2c5b5ade7fc6398f6e
    Depends: libosmocore I1770787e697906322ce5815fcaadba06c01ddee9

--- src/osmo-mgw/mgw_main.c
+++ src/osmo-mgw/mgw_main.c
@@ -280,7 +280,7 @@ int main(int argc, char **argv)
 
 	/* start telnet after reading config for vty_get_bind_addr() */
 	rc = telnet_init_dynif(tall_bsc_ctx, NULL,
-			       vty_get_bind_addr(), OSMO_VTY_PORT_BSC_MGCP);
+			       vty_get_bind_addr(), OSMO_VTY_PORT_MGW);
 	if (rc < 0)
 		return rc;
 
