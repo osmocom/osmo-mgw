086c3f3c674a276205a793300c80802d2355cabd
Neels Hofmeyr <neels@hofmeyr.de>
2018-03-22 14:56:20 +0100

    cosmetic: mgcp, legacy_mgcp: drop unused vty.h definitions
    Change-Id: I38e279ed175259e6d36c0e69d6a352506face014

--- include/osmocom/mgcp/vty.h
+++ include/osmocom/mgcp/vty.h
@@ -1,31 +1,8 @@
-#ifndef OPENBSC_VTY_H
-#define OPENBSC_VTY_H
+#pragma once
 
-#include <osmocom/vty/vty.h>
-#include <osmocom/vty/buffer.h>
 #include <osmocom/vty/command.h>
-
-struct gsm_network;
-struct vty;
-
-void openbsc_vty_print_statistics(struct vty *vty, struct gsm_network *);
-
-struct buffer *vty_argv_to_buffer(int argc, const char *argv[], int base);
-
-extern struct cmd_element cfg_description_cmd;
-extern struct cmd_element cfg_no_description_cmd;
 
 enum mgcp_vty_node {
 	MGCP_NODE = _LAST_OSMOVTY_NODE + 1,
 	TRUNK_NODE,
 };
-
-struct log_info;
-int bsc_vty_init(struct gsm_network *network);
-int bsc_vty_init_extra(void);
-
-void msc_vty_init(struct gsm_network *msc_network);
-
-struct gsm_network *gsmnet_from_vty(struct vty *vty);
-
-#endif
