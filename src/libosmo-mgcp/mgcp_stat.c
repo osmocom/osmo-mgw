osmocom/osmo-mgw

commit f936e10f07b7a55d53a6f426b80364f6d368c5a0

stats: make sanitizers happy
The test expects wrapping here, but the undefined sanitizer is not happy
with that, so disable it.

Change-Id: I59ec65519ea028d4628ba4b56c939aef70794abf

src/libosmo-mgcp/mgcp_stat.c
@@ -30,6 +30,11 @@
 #include <osmocom/mgcp/mgcp_endp.h>
 
 /* Helper function for mgcp_format_stats_rtp() to calculate packet loss */
+#if defined(__has_attribute)
+#if __has_attribute(no_sanitize)
+__attribute__((no_sanitize("integer")))
+#endif
+#endif
 void calc_loss(struct mgcp_conn_rtp *conn, uint32_t *expected, int *loss)
 {
 	struct mgcp_rtp_state *state = &conn->state;

commit a0b69f189617278fb2e23fde0e92f328183109dd

Fix attribute parsing on gcc 11.1.0
Fixes following compilation error:
"osmo-mgw/src/libosmo-mgcp/mgcp_stat.c:39:1: error: ‘integer’ attribute directive ignored"

Change-Id: Ia5167068abe8a22cd35a833396cbd7cb531c7e83
Fixes: f936e10f07b7a55d53a6f426b80364f6d368c5a0

src/libosmo-mgcp/mgcp_stat.c
@@ -33,7 +33,7 @@
 /* Helper function for mgcp_format_stats_rtp() to calculate packet loss */
 #if defined(__has_attribute)
 #if __has_attribute(no_sanitize)
-__attribute__((no_sanitize("integer")))
+__attribute__((no_sanitize("undefined")))
 #endif
 #endif
 void calc_loss(struct mgcp_conn_rtp *conn, uint32_t *expected, int *loss)