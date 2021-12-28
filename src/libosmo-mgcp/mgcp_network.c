osmocom/osmo-mgw

commit ebb05c1f90e97d0795230e90ea49af7d084dc9fe

mgcp_network.c: Set proper CRC Header for ACK Initialization
Discovered while debugging wireshark CRC calculation and implementing
new IuUP code in libosmocore.

Change-Id: Ic8350d1f9a9e5dcefeb787462d267bfac08d778f

src/libosmo-mgcp/mgcp_network.c
@@ -1225,6 +1225,7 @@ int mgcp_send(struct mgcp_endpoint *endp, int is_rtp, struct osmo_sockaddr *addr
 				if (data[0] == 0xe0) {
 					data[0] = 0xe4;
 					data[1] = 0x00;
+					data[2] = 0x09; /* Patch CRC Header to adapt to new header above */
 					rtp_state->patched_first_rtp_payload = true;
 					LOGPENDP(endp, DRTP, LOGL_DEBUG,
 						 "Patching over first two bytes"

commit e30820228539fa1cf168e3dc35ba25a42251f613

mgcp_network.c: Fix byte alignment of CRC Header for ACK Initialization
The Header CRC field is 6 bits, not 8, and spans bits 7-2.

Fixes: ebb05c1f90e97d0795230e90ea49af7d084dc9fe
Change-Id: I9a8179813d451948bfa02443894fdd2313dfc4a0

src/libosmo-mgcp/mgcp_network.c
@@ -1225,7 +1225,7 @@ int mgcp_send(struct mgcp_endpoint *endp, int is_rtp, struct osmo_sockaddr *addr
 				if (data[0] == 0xe0) {
 					data[0] = 0xe4;
 					data[1] = 0x00;
-					data[2] = 0x09; /* Patch CRC Header to adapt to new header above */
+					data[2] = (0x09 << 2); /* Patch CRC Header to adapt to new header above */
 					rtp_state->patched_first_rtp_payload = true;
 					LOGPENDP(endp, DRTP, LOGL_DEBUG,
 						 "Patching over first two bytes"