b5618dade52b4a51f4aa755b00de75da4df721f1
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2024-02-16 01:53:47 +0100

    add libosmo-sdp [1/n] add fmtp.h
    Add new noinst-library libosmo-sdp.la.
    
    It will be used by
    - osmo-mgw
    - libosmo-mgcp-client
    - callers of libosmo-mgcp-client may use SDP message parsing (osmo-msc)
    
    The API will, with upcoming patches:
    - parse and compose SDP fmtp strings
    - manage SDP codecs and codec lists (like 'AMR:octet-align=1#112')
    - parse and compose entire SDP messages
    
    In this patch: parse and compose SDP fmtp strings.
    
    Add libosmo-sdp build fu.
    Add sdp/fmtp.h and sdp/sdp_strings.h.
    Add tests/sdp/sdp_fmtp_test.c.
    
    Change-Id: I6128852f4d249e90319f43d6cd6ed0a9a2ed0430

---
+++ include/osmocom/sdp/sdp_strings.h
@@ -0,0 +1,36 @@
+/* Central definition of string tokens used for parsing and composing SDP messages */
+#pragma once
+
+#define OSMO_SDP_STR_MEDIA "m"
+#define OSMO_SDP_STR_ATTRIB "a"
+#define OSMO_SDP_STR_TIME_ACTIVE "t"
+
+#define OSMO_SDP_STR_RTPMAP "rtpmap"
+#define OSMO_SDP_STR_FMTP "fmtp"
+#define OSMO_SDP_STR_PTIME "ptime"
+
+/*! "a=foo:" */
+#define OSMO_SDP_A_PREFIX(STR) OSMO_SDP_STR_ATTRIB "=" STR ":"
+
+/* Media Direction Attributes "a=recvonly", "a=sendrecv", "a=sendonly", "a=inactive" RFC-8866 6.7. */
+#define OSMO_SDP_STR_RECVONLY "recvonly"
+#define OSMO_SDP_STR_SENDRECV "sendrecv"
+#define OSMO_SDP_STR_SENDONLY "sendonly"
+#define OSMO_SDP_STR_INACTIVE "inactive"
+
+/* AMR related tokens */
+
+#define OSMO_SDP_STR_AMR_OCTET_ALIGN "octet-align"
+
+/*! "octet-align=1" */
+#define OSMO_SDP_STR_AMR_OCTET_ALIGN_1  OSMO_SDP_STR_AMR_OCTET_ALIGN "=1"
+
+/*! "octet-align=0".
+ * According to spec [1], "octet-align=0" is identical to omitting 'octet-align' entirely. In Osmocom practice, whether
+ * or not "octet-align=0" is present can make a big difference for osmo-mgw versions 1.12 and older, which do not heed
+ * [1].
+ *
+ * spec [1]: RFC4867, see details in description of osmo_sdp_fmtp_amr_is_octet_aligned().
+ */
+#define OSMO_SDP_STR_AMR_OCTET_ALIGN_0  OSMO_SDP_STR_AMR_OCTET_ALIGN "=0"
+
