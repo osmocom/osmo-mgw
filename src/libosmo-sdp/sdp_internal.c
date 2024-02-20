9354f957b2fa2d6adbd7dd1ec857292f8bd5803b
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2024-02-20 06:25:56 +0100

    add libosmo-sdp: add sdp_msg.h,.c
    Change-Id: Id53c2c6eea3726f63a1399ae985f8aa3344e32c8

--- src/libosmo-sdp/sdp_internal.c
+++ src/libosmo-sdp/sdp_internal.c
@@ -22,6 +22,9 @@
  */
 
 #include <stddef.h>
+#include <ctype.h>
+#include <limits.h>
+#include <errno.h>
 
 #include <osmocom/core/talloc.h>
 #include <osmocom/core/utils.h>
@@ -44,4 +47,75 @@ void token_copy(void *ctx, char **dst, const struct token *t)
 	*dst = talloc_size(ctx, len + 1);
 	osmo_strlcpy(*dst, t->start, len + 1);
 	talloc_set_name_const(*dst, *dst);
+}
+
+const char *token_chr(const struct token *src, char c)
+{
+	const char *pos;
+	for (pos = src->start; *pos && pos < src->end; pos++) {
+		if (*pos == c)
+			return pos;
+	}
+	return NULL;
+}
+
+const char *token_chrs(const struct token *src, const char *chrs)
+{
+	const char *pos;
+	for (pos = src->start; pos && *pos && pos < src->end; pos++) {
+		if (strchr(chrs, *pos))
+			return pos;
+	}
+	return NULL;
+}
+
+void token_next(struct token *t, const char *str, const char *end, const char *separators)
+{
+	t->start = str;
+	while (strchr(separators, *t->start) && t->start < end)
+		t->start++;
+	t->end = t->start;
+	while (!strchr(separators, *t->end) && t->end < end)
+		t->end++;
+}
+
+/* Convert the token to an integer, and return the character afer the integer string that was parsed.
+ * The difference to osmo_str_to_int64() is that this guarantees to only access the memory from t->start to t->end. */
+const char *token_to_int64(int64_t *result, const struct token *t, int base, int min_val, int max_val)
+{
+	/* copy the number section over to a temporary buffer, because t->end may terminate the string anywhere, and
+	 * functions like strtoll (which osmo_str_to_int64() uses) are not able to stay within a strict buffer length
+	 * unless the string is zero terminated at the buffer boundary. */
+	char buf[32];
+	const char *int_end = t->start;
+	if (int_end < t->end && *int_end == '-')
+		int_end++;
+	while (int_end < t->end && isdigit(*int_end))
+		int_end++;
+	if (int_end - t->start >= sizeof(buf))
+		return NULL;
+	osmo_strlcpy(buf, t->start, int_end - t->start + 1);
+	if (osmo_str_to_int64(result, buf, base, min_val, max_val) != 0)
+		return NULL;
+	return int_end;
+}
+
+/* Convenience: like token_to_int64() but with a plain int. */
+const char *token_to_int(int *result, const struct token *t, int base, int min_val, int max_val)
+{
+	int64_t val;
+	const char *rc = token_to_int64(&val, t, base, min_val, max_val);
+	if (val < INT_MIN) {
+		if (result)
+			*result = INT_MIN;
+		return NULL;
+	}
+	if (val > INT_MAX) {
+		if (result)
+			*result = INT_MAX;
+		return NULL;
+	}
+	if (result)
+		*result = (int)val;
+	return rc;
 }
