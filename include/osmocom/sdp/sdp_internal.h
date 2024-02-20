58be6e9ec3ccfdd71a7885ac37132e01349a7950
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2024-02-20 06:25:56 +0100

    add libosmo-sdp: add sdp_msg.h,.c
    Change-Id: Id53c2c6eea3726f63a1399ae985f8aa3344e32c8

--- include/osmocom/sdp/sdp_internal.h
+++ include/osmocom/sdp/sdp_internal.h
@@ -9,3 +9,10 @@ struct token {
 /* Copy a string from [start,end[, return as talloc allocated under ctx in *dst.
  * If *dst is non-NULL, talloc_free(*dst) first. */
 void token_copy(void *ctx, char **dst, const struct token *t);
+
+const char *token_chr(const struct token *src, char c);
+const char *token_chrs(const struct token *src, const char *chrs);
+void token_next(struct token *t, const char *str, const char *end, const char *separators);
+
+const char *token_to_int64(int64_t *result, const struct token *t, int base, int min_val, int max_val);
+const char *token_to_int(int *result, const struct token *t, int base, int min_val, int max_val);
